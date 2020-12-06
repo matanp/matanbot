ae6a3aa184593fee102a5de3898e53a550b5c28a
matanp <mgp64@cornell.edu>
2020-12-06 02:51:52 -0800

    initial commit of handling twitch messages and basic bot brain

---
+++ twitch_main.js
@@ -0,0 +1,53 @@
+'use strict';
+require('dotenv').config();
+
+const { exit } = require('process');
+
+//twitch client
+const tmi = require('tmi.js');
+
+//and we need the bot
+const { bot } = require('./bot_brain.js');
+
+// Define configuration options from env file
+const opts = {
+  identity: {
+    username: process.env.USER,
+    password: process.env.OATH
+  },
+  channels: [
+    process.env.CHANNEL
+  ]
+};
+
+// Create a Twitch client with options
+const client = new tmi.client(opts);
+
+// Register event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+// target is the twitch channel the message is coming from
+// context is information about the user -- see example_context
+// msg is the actual message
+// self is a boolean of if the message is coming from this client
+function onMessageHandler (target, context, msg, self) {
+    if (self) { return; }
+    //console.log(target);
+    //console.log(context);
+    console.log(msg);
+    let response = bot(context, msg);
+    if (response) {
+      client.say(target, response);
+    }
+
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+}
