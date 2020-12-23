f39b9b4c38400d8bcc2e38d42108b521c2d842f1
matanp <mgp64@cornell.edu>
2020-12-22 17:01:14 -0800

    minor style changes

--- bot_brain.js
+++ bot_brain.js
@@ -3,30 +3,30 @@
 var count = 0;
 
 // Called every time a message comes in
-function message_main (context, msg) {
+function message_main (user_info, user_msg) {
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const commandName = user_msg.trim();
 
   const upper_case = commandName.toUpperCase();
 
   //random number 1 to 10
-  var r_num = Math.floor((Math.random() * 10) + 1);
-  var msg_out = "";
+  const random_num = Math.floor((Math.random() * 10) + 1);
+  let msg_out = "";
 
   if (upper_case.search("DISCORD") != -1) {
     return "Did someone say discord? Join Matan's discord to get updates on stream schedule, juggling advice, hangout and all around have a good time! https://discord.gg/bNUaFRE";
   }
   if (upper_case.search("MATANBOT") != -1) {
     count = count + 1;
-    if (r_num > 9) {
+    if (random_num > 9) {
       msg_out = "matanbot is currently under development, please excuse the lack of functionality"
-    } else if (r_num > 8){
+    } else if (random_num > 8){
       msg_out = "I'm so glad someone recognizes my potential as being the best bot on the channel"
-    } else if (r_num > 6) {
+    } else if (random_num > 6) {
       msg_out = "I am a juggle bot, that is all I am"
     }
     else {
-      msg_out = "hi " + context.username + "!"
+      msg_out = "hi " + user_info.username + "!"
     }
     if (count % 11 == 0) {
       return `yall said my name ${count.toString()} times? wow!`;
--- twitch_main.js
+++ twitch_main.js
@@ -1,16 +1,15 @@
 'use strict';
+/* global process */
 require('dotenv').config();
 
-const { exit } = require('process');
-
 //twitch client
-const tmi = require('tmi.js');
+const twitch_client = require('tmi.js');
 
 //and we need the bot
 const { bot } = require('./bot_brain.js');
 
 // Define configuration options from env file
-const opts = {
+const client_config = {
   identity: {
     username: process.env.USER,
     password: process.env.OATH
@@ -21,7 +20,7 @@ const opts = {
 };
 
 // Create a Twitch client with options
-const client = new tmi.client(opts);
+const client = new twitch_client.client(client_config);
 
 // Register event handlers (defined below)
 client.on('message', onMessageHandler);
@@ -30,19 +29,21 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+/*
 // Called every time a message comes in
-// target is the twitch channel the message is coming from
-// context is information about the user -- see example_context
-// msg is the actual message
-// self is a boolean of if the message is coming from this client
-function onMessageHandler (target, context, msg, self) {
-    if (self) { return; }
+ target_channel is the twitch channel the message is coming from
+ user_info is information about the user -- see example_context
+ user_msg is the actual message
+ from_self is a boolean of if the message is coming from this client
+*/
+function onMessageHandler (target_channel, user_info, user_msg, from_self) {
+    if (from_self) { return; }
     //console.log(target);
     //console.log(context);
-    console.log(msg);
-    let response = bot(context, msg);
+    console.log(user_msg);
+    let response = bot(user_info, user_msg);
     if (response) {
-      client.say(target, response);
+      client.say(target_channel, response);
     }
 
 }
