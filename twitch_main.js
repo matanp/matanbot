77ebc759e878168522e02b379c35deedbdc3ef0c
matanp <mgp64@cornell.edu>
2021-01-08 00:06:18 -0800

    added fire obs background made tab width consistent to 4 across all files

--- twitch_main.js
+++ twitch_main.js
@@ -15,11 +15,11 @@ const channelOut = process.env.CHANNEL;
 
 // Define configuration options from env file
 const client_config = {
-  identity: {
-    username: process.env.USER,
-    password: process.env.OATH,
-  },
-  channels: [process.env.CHANNEL],
+    identity: {
+        username: process.env.USER,
+        password: process.env.OATH,
+    },
+    channels: [process.env.CHANNEL],
 };
 
 // Create a Twitch client with options
@@ -47,40 +47,40 @@ connectOBSWebsocket(); //connect to obs
  from_self is a boolean of if the message is coming from this client
 */
 function onMessageHandler(target_channel, user_info, user_msg, from_self) {
-  if (from_self) {
-    return;
-  }
-  console.log(user_msg);
-
-  //handle timer logic
-  for (let timer of timer_data.timers) {
-    timer.countLines = timer.countLines + 1;
-    if (timer.prioritizeLines && timer.countLines > timer.minLines) {
-      timer.countLines = 0;
-      client.say(channelOut, timer.message);
+    if (from_self) {
+        return;
     }
-  }
+    console.log(user_msg);
 
-  //pass the message into the bot
-  let response_promise = bot.message_main(user_info, user_msg);
-  response_promise.then(
-    (result) => {
-      if (result) client.say(target_channel, result);
-    },
-    (error) => console.log(error)
-  );
+    //handle timer logic
+    for (let timer of timer_data.timers) {
+        timer.countLines = timer.countLines + 1;
+        if (timer.prioritizeLines && timer.countLines > timer.minLines) {
+            timer.countLines = 0;
+            client.say(channelOut, timer.message);
+        }
+    }
+
+    //pass the message into the bot
+    let response_promise = bot.message_main(user_info, user_msg);
+    response_promise.then(
+        (result) => {
+            if (result) client.say(target_channel, result);
+        },
+        (error) => console.log(error)
+    );
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-  for (let timer of timer_data.timers) {
-    timer.countLines = 0;
-    setInterval(() => {
-      if (timer.countLines >= timer.minLines) {
+    console.log(`* Connected to ${addr}:${port}`);
+    for (let timer of timer_data.timers) {
         timer.countLines = 0;
-        client.say(channelOut, timer.message);
-      }
-    }, timer.time * 1000);
-  }
+        setInterval(() => {
+            if (timer.countLines >= timer.minLines) {
+                timer.countLines = 0;
+                client.say(channelOut, timer.message);
+            }
+        }, timer.time * 1000);
+    }
 }
