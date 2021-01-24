7ad65de13b9366b875785132943d3fd8b3126e05
matanp <mgp64@cornell.edu>
2021-01-24 14:21:41 -0800

    working twitch chat component and websocket receiver (#10)
    Front end react app listening to websocket connection for twitch chat messages.

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TwitchChat from "./components/TwitchChat";
+
+function App() {
+    return (
+        <div className="App">
+            <TwitchChat />
+        </div>
+    );
+}
+
+export default App;
