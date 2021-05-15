f4dec7d985655d982bb60d0218ef4f4be2caf11f
matanp <mgp64@cornell.edu>
2021-05-15 01:36:46 -0700

    listening to channel points, handling pings and pongs and reconnects

--- src/backend/twitch_events_ws.ts
+++ src/backend/twitch_events_ws.ts
@@ -1,86 +1,84 @@
 //https://dev.twitch.tv/docs/pubsub
 require("dotenv").config();
-const express = require('express');
-import WebSocket from 'ws';
-const fetch = require("node-fetch");
+import WebSocket from "ws";
 
-const app = express();
+let ws: WebSocket;
 
-app.get('/auth/twitch', (req : any, res:any ) => {
-    console.log('expressss');
-    console.log(req);
-    console.log(res);
+interface PubSubMessage {
+  type: String;
+  nonce: String;
+  data: {
+    topics: String[];
+    auth_token: String;
+  };
+}
 
-    return 'hello';
-});
+interface PubSubResponse {
+  type: String;
+  data: {
+    topic: String;
+    message: String;
+  };
+}
 
-app.post('/auth/twitch', (req: any, res: any) => {
-  console.log('post');
-  console.log(req);
-});
 
-app.listen(8080, () => console.log('listening'));
+let connectionTries : number = 0;
 
-let ws : WebSocket;
+//figure out next exponential backoff
+//adds some random jitter, returns -1 if over 2 minutes
+function exponentialBackoff(iter: number) : number {
+  let backoffTime: number =  (Math.pow(2,iter) * 1000) + (Math.random() * 1000);
+
+  if(backoffTime > 2 * 60 * 1000) {
+    return -1;
+  }
+
+  return backoffTime;
+}
 
 connect();
 
-let clientId = process.env.CLIENT_ID;
-let redirectURI = "localhost:8080/auth/twitch";
-let sessionID = randomString(15);
-let scope = "chat:read";
-
-let url =
-  "https://id.twitch.tv/oauth2/authorize" +
-  "?response_type=token" +
-  "&client_id=" +
-  clientId +
-  "&redirect_uri=" +
-  redirectURI +
-  "&state=" +
-  sessionID +
-  "&scope=" +
-  scope;
-
-const authorize = async () => {
-  let response = await fetch(url);
-  //console.log(response);
-//   console.log(response);
-//   console.log(response.url);
-//   console.log(response.headers);
-};
-
-setTimeout(() => authorize(), 1000);
-
-function randomString(length: number) {
-  var text = "";
-  var possible =
+function randomString(length: number): String {
+  let text: String = "";
+  let possible: String =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < length; i++) {
+  for (let i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 }
 
 //all wrapped in a function to handle reconnecting
-function connect() {
+function connect(): void {
+  let needsReconnect: Boolean = false;
+
   ws = new WebSocket("wss://pubsub-edge.twitch.tv");
 
-  //TODO reconnect if don't receive PONG back from PING
-  ws.onopen = function (event) {
+  function reconnect(): void {
+    ws.close();
+    connect();
+  }
+
+  ws.onopen = function (event: WebSocket.OpenEvent): void {
     console.log("opened");
+    connectionTries = 0; //connnection successful, reset count
     //ping server every 4.5 - 4.55 minutes
     //randomness since Twitch asked nicely
-    (function heartbeat() {
+    (function heartbeat(): void {
       let rand = 1000 * 60 * 4.5 + Math.random() * 5000;
-      setTimeout(function () {
+      setTimeout(function (): void {
         ws.send(`{ "type": "PING" }`);
+        needsReconnect = true; //set this to false once recieved PONG
+        setTimeout(function (): void {
+          if (needsReconnect) {
+            reconnect();
+          }
+        }, 10 * 1000); //if after 10 seconds no PONG, reconnect
         heartbeat();
       }, rand);
     })();
 
-    //stuck on getting channel id of my channel, this one incorrect
-    const listen_to_topics = {
+    const listen_to_topics: PubSubMessage = {
       type: "LISTEN",
       nonce: randomString(17),
       data: {
@@ -92,12 +90,27 @@ function connect() {
     ws.send(JSON.stringify(listen_to_topics));
   };
 
-  ws.onerror = function (error) {
+  ws.onerror = function (error: WebSocket.ErrorEvent): void {
     console.log(error);
+    //connection failed, try to reconnect on exponentialBackoff
+    let connectionRetryTime = exponentialBackoff(connectionTries)
+    if(connectionRetryTime !== -1) { setTimeout(() => connect(), connectionRetryTime); }
+    connectionTries++;
+
   };
 
-  //TODO handle RECONNECT messages from server
-  ws.onmessage = function (event) {
+  ws.onmessage = function (event: WebSocket.MessageEvent): void {
     console.log(String(event.data).trim());
+    const message: PubSubResponse = JSON.parse(String(event.data));
+
+    //recieved reconnect from Twitch
+    if (message.type === "RECONNECT") {
+      reconnect();
+    }
+
+    if (message.type === "PONG") {
+      //received PONG after PING, no need to reconnect
+      needsReconnect = false;
+    }
   };
 }
